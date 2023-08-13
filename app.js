2243bab609c332f7aec7eb1422e85ef0f37456f4
Nomanshabbir <135643492+Nomanshabbir1@users.noreply.github.com>
2023-08-13 07:34:04 -0700

    assignment 6-9 done

---
+++ app.js
@@ -0,0 +1,36 @@
+// QUESTION # 1
+
+// var originalNum = (10)
+
+// orginalNum = (++originalNum)
+
+// alert(orginalNum)
+
+// orginalNum = (orginalNum + 1)
+
+// alert(orginalNum)
+
+// orginalNum = --orginalNum
+
+// alert(orginalNum)
+
+// orginalNum= orginalNum - 1
+
+// alert(orginalNum)
+
+//QUESTION # 2
+
+// var a = 2
+// var b = 1
+
+// var newvar = (--a - --b + ++b + b--)
+
+// // a= 1
+// // b=0
+
+// alert(newvar)
+
+// QUESTION # 3
+
+var userName =prompt("Enter Your Full Name")
+alert("Hello ,"+ userName + "! Welcome !")
